6272e7709c4be7b9572a17de0223753655c01d26
unknown <mortensonbrock@gmail.com>
2024-02-24 03:24:26 -0700

    inital layout for about page

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { CurrentsService } from 'src/services/currents.service';
 
 @Component({
   selector: 'about-me-component',
   templateUrl: './about-me.component.html',
   styleUrls: ['./about-me.component.scss'],
 })
-export class AboutMeComponent implements OnInit {
+export class AboutMeComponent implements OnInit, OnDestroy {
+  public isMobile: boolean = false;
 
-  constructor() { }
+  private mobileSubscription: Subscription = new Subscription();
+
+  constructor(private currentsService: CurrentsService) { }
 
   public ngOnInit() {
+    this.mobileSubscription = this.currentsService.getIsMobile().subscribe((bool) => {
+      this.isMobile = bool;
+    });
+  }
+
+  public ngOnDestroy(): void {
+    this.mobileSubscription.unsubscribe();
   }
 
 }
