f3b623c4b8272845d341bbc5059bedb90d74a84d
unknown <mortensonbrock@gmail.com>
2024-02-23 21:33:27 -0700

    commit

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.scss'],
 })
 export class ProjectsComponent implements OnInit {
-
+  public test = null;
+  
   constructor() { }
 
   public ngOnInit() {
